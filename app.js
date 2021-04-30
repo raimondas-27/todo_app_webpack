60dd9253c521a2b346f090d85095880280d52cdc
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-30 10:23:15 +0300

    git init

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict"; // here we go again
+
+// reikalingos nuorodo i elementus kuriuos valdysime
+const inputEl = document.getElementById("todo-input");
+const addTodoBtnEl = inputEl.nextElementSibling;
+const mainListUlEl = document.querySelector(".list");
