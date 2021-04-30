cc382cb4d30704d1d6a85a1365950515eeecb066
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-30 10:37:06 +0300

    simpleTodo

--- app.js
+++ app.js
@@ -4,3 +4,21 @@
 const inputEl = document.getElementById("todo-input");
 const addTodoBtnEl = inputEl.nextElementSibling;
 const mainListUlEl = document.querySelector(".list");
+
+// 0 mygtukas turi laukti paspaudimo ir sureaguoti kai paspausim
+addTodoBtnEl.addEventListener("click", function () {
+  //   console.log("paspaudei");
+  // 1 paimam ka ivede vartojoas i input
+  const ivestaReiksme = inputEl.value;
+  console.log(ivestaReiksme);
+
+  // 2 dedam i sarasa kaip li el
+  // sukuriam nauja li
+  const naujasLiEl = document.createElement("li");
+  // ideam vartotojo reiksme
+  naujasLiEl.textContent = ivestaReiksme;
+  // idedam i sarasa nauja li el su tekxtu
+  mainListUlEl.appendChild(naujasLiEl);
+  // isvalyti ivesties lauka po to kai pridejom todo
+  inputEl.value = "";
+});
